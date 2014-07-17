2ddb903dbbfc84b6af833e55571f5e7673679e83
Sergii Mertsalov & Nuatu Tseggai <student@epicodus.com>
2014-07-17 16:48:32 -0700

    Add functionality added

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+var Category = {
+};
+var Purchase = {
+description: "",
+amount: 0
+};
+
+
+$(document).ready(function() {
+var currentCategory;
+
+$("#add-btn").click(function(event) {
+  event.preventDefault();
+  var newName = $("input#new-category").val().toUpperCase();
+  var newCategory = Object.create(Category);
+  newCategory.name = newName;
+  newCategory.purchases = [];
+  console.log(newCategory)
+  $("div#categories").append("<p>" + newName + "</p>");
+  $("input#new-category").val("");
+  $("#purchases-table .empty").empty();
+
+
+  $("#categories p").last().click(function(){
+    $("#purchases h2").text(newName);
+    $("#add-name").text(newName);
+    $("#purchases-table .empty").empty();
+    // $("#add-purchase").text(newName);
+    currentCategory = newCategory;
+
+    //empty all expenses
+    //foreach on currentCategpory.expenses
+    currentCategory.purchases.forEach(function(purchase){
+
+        $("#purchases-table tbody").append("<tr class='empty'><td>" + purchase.description + "</td><td>" + purchase.amount + "</td></tr>")
+    });
+  });
+});
+
+
+$("form#add-purchase").submit(function(event){
+  event.preventDefault();
+
+  var newDescription = $("input#input-description").val();
+  var newAmount = parseFloat($("input#input-amount").val());
+  var newPurchase = Object.create(Purchase);
+  newPurchase.description = newDescription;
+  newPurchase.amount = newAmount;
+  console.log(newPurchase);
+  currentCategory.purchases.push(newPurchase);
+
+  $("#purchases-table tbody").append("<tr class='empty'><td>" + newDescription + "</td><td>" + newAmount + "</td></tr>")
+
+  $("input#input-description").val("");
+  $("input#input-amount").val("");
+});
+
+});
